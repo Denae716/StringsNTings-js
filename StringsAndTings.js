453d9a7874f0b6e03e759434a7262493cef302d5
Denae716 <denaebrown73@yahoo.com>
2020-12-19 14:23:17 -0500

    12192020 class practice

--- StringsAndTings.js
+++ StringsAndTings.js
@@ -1,29 +1,74 @@
 class StringsAndTings {
     
-    // @return string with identical content, and the first character capitalized
     camelCase(str){
-        return null;
-    }
-
+        let output = ""
+        for(let i = 0; i < str.length; i ++){
+          if( i === 0 || str[i-1] === " "){
+            output += str[i].toUpperCase()
+          }
+          else {
+            output += str[i]
+          }
+        }
+        return output;
+      }
+  
+      //@return string in reverse order
+      reverseString(str){
+        let output = ""
+        for (let i = str.length -1; i >= 0; i--){
+          output += str[i]
+        }
+          return output;
+      }
+  
     //@return string with identical contents, with each word individually in reverse order
-    reverseString(str){
-        return null;
-    }
-
-    // @return string with identical contents, in reverse order, with first character capitalized
-    reverseThenCamelCase(str){
-        return null;
-    }
-
-    // @return string with identical contents excluding first and last character
-    removeFirstAndLastCharacter(str){
-        return null;
-    }
-
-    // @return string with identical characters, each with opposite casing
-    invertCasing(str){
-        return null;
-    }
-}
+      reverseWords(str){
+        let output = ""
+        let words = str.split(" ")
+        for(let i = 0; i <words.length; i ++){
+          if (i ===words.length -1){
+          output += this.reverseString(words[i])  
+         } else{
+          output += this.reverseString(words[i])+ " "}   
+        }
+        return output;
+      }
+  
+      // @return string with identical contents, in reverse order, with first character capitalized
+      reverseThenCamelCase(str){
+        let output = this.reverseString(str)
+        output = this.camelCase(output)
+          return output;
+      }
+  
+      // @return string with identical contents excluding first and last character
+      removeFirstAndLastCharacter(str){
+        let output = "";
+        for (let i = 0; i < str.length; i++){ 
+          if(i !== 0 && i !== str.length - 1){ 
+             output += str[i]
+  
+          }
+  
+        }
+         
+          return output;
+      }
+  
+      // @return string with identical characters, each with opposite casing
+      invertCasing(str){
+        let output = ""
+        for(let i = 0; i < str.length; i ++){
+          if(str[i] === str[i].toUpperCase()){
+            output += str[i].toLowerCase()
+          }
+          else (str[i] === str[i].toLowerCase()){
+            output += str[i].toUpperCase()
+          }
+        }
+          return output;
+      }
+  }
 
 module.exports = StringsAndTings;
